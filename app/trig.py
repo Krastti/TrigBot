33058565f93e9b57e92d47af68f7cca1525db5a5
Yaroslav Katanov <salcev77@gmail.com>
2024-09-09 16:17:59 +0200

    trig

--- app/trig.py
+++ app/trig.py
@@ -25,7 +25,7 @@ help_text = [
   ("sin(2x) = 2sin(x)cos(x)"),
   ("cos(2x) = cos^2(x)-sin^2(x)"),
   ("cos(2x) = 2cos^2(x)-1"),
-  ("cos(2x) = 1-2sin^2(x)")
+  ("cos(2x) = 1-2sin^2(x)"),
   (""),
 ]
 
